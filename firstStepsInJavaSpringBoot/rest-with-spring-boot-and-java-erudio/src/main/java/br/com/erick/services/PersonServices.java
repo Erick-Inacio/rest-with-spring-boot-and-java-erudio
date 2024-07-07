2c9e8b4a0002aa3254881b3a3086d35dcb36b4b2
ErickInacio <erick.de.faria@hotmail.com>
2024-07-07 14:46:06 -0300

    Session 10 finished

--- firstStepsInJavaSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erick/services/PersonServices.java
+++ firstStepsInJavaSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erick/services/PersonServices.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.erick.data.vo.v1.PersonVO;
+import br.com.erick.data.vo.v2.PersonVOV2;
 import br.com.erick.exceptions.ResourceNotFoundException;
 import br.com.erick.mapper.DozerMapper;
+import br.com.erick.mapper.custom.PersonMapper;
 import br.com.erick.model.Person;
 import br.com.erick.repositories.PersonRepository;
 
@@ -18,6 +20,9 @@ public class PersonServices {
 	
 	@Autowired
 	PersonRepository repository;
+	
+	@Autowired
+	PersonMapper mapper;
 	
 	public List<PersonVO> findAll() {
 		
@@ -42,6 +47,16 @@ public class PersonServices {
 		
 		var entity = DozerMapper.parseObject(person, Person.class);
 		var vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+		
+		return vo;
+		
+	}
+	public PersonVOV2 createV2(PersonVOV2 person) {		
+		
+		logger.info("Create one person with V2!");
+		
+		var entity = mapper.convertVoToEntity(person);
+		var vo = mapper.convertEntityToVo(repository.save(entity));
 		
 		return vo;
 		
