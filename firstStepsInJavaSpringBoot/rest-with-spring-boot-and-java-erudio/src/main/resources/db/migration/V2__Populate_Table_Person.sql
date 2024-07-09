8fbeb53c81380d44cb17de7a942786455eb3538b
ErickInacio <erick.de.faria@hotmail.com>
2024-07-09 01:02:57 -0300

    session finished 09.07.24

---
+++ firstStepsInJavaSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(3, 'São Paulo', 'Fabricio', 'Male', 'inácio'),
+	(4, 'Araçoiaba da Serra', 'Erick', 'Male', 'Faria Inácio'),
+	(5, 'São Paulo', 'Erick', 'Male', 'inácioChique'),
+	(6, 'São Paulo', 'Joice', 'Female', 'Faria');
